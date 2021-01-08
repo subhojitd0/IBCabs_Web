ee15e0831ad1358ddad0abecf49d96d16b342881
SouradeepG <41611584+SouradeepG@users.noreply.github.com>
2021-01-08 21:32:24 +0530

    fix
    fix

--- src/app/features/rental-detail/rental-detail.component.ts
+++ src/app/features/rental-detail/rental-detail.component.ts
@@ -162,8 +162,8 @@ export class RentalDetailComponent implements OnInit {
       } 
       this.apiService.post(PARTY_HEAD_API, json).then((res: any)=>{ 
         this.rentalAdd.ginbeforekm = res.kmin;
-        this.rentalAdd.goutbeforetime = "00:" + res.garagein;
-        this.rentalAdd.ginbeforetime = "00:" + res.garageout;
+        this.rentalAdd.goutbeforetime = res.garagein;
+        this.rentalAdd.ginbeforetime = res.garageout;
         this.rentalAdd.goutbeforekm = res.kmout;
         this.rentalAdd.dutytime = res.starttime ? res.starttime.substr(0,5) : "";
       });
