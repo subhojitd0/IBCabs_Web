ec238063e4252597b9279e57a59fe6d408a8fb2d
SouradeepG <41611584+SouradeepG@users.noreply.github.com>
2021-01-30 01:59:51 +0530

    bill part 2
    bill part 2

--- src/app/features/bills/monthly-bill-a/monthly-a.component.ts
+++ src/app/features/bills/monthly-bill-a/monthly-a.component.ts
@@ -7,13 +7,17 @@ import {OWNER_API, PARTY_HEAD_API} from '../../../../shared/services/api.url-hel
 import {MatDialog} from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
+import jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 import { ROUTE_CAR, ROUTE_OWNER } from 'src/shared/constants/constant';
 
 export interface Owner {
-  ownercode: string;
-  ownername: number;
-  assignedcar: string;
-  option: string;
+  sl: string;
+  dutydate: number;
+  carno: string;
+  hour: string;
+  km: number;
+  parking: string;
 }
 
 @Component({
@@ -22,26 +26,46 @@ export interface Owner {
   styleUrls: ['./monthly-a.component.css']
 })
 export class MonthlyBillAComponent implements OnInit {
-
-  isPack:boolean=true;
-  isSlab:boolean=false;
+  billdetails: any;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-  displayedColumns: string[] = ['ownercode', 'ownername', 'assignedcar'];
+  displayedColumns: string[] = ['sl', 'dutydate', 'carno', 'hour', 'km', 'parking'];
   dataSource: MatTableDataSource<Owner>;
   constructor(private router: Router,private apiService: ApiService, public dialog: MatDialog, private toastr: ToastrService) {
     
    }
-   ngOnInit() : void {
-    var json = 
-    {
-      "mode": 0
-    };
-    this.apiService.post(OWNER_API, json).then((res: any)=>{ 
-      debugger;
-      const owner: Owner[] = res.result;
-      this.dataSource = new MatTableDataSource(owner);
-    });
+   ngOnInit(){
+    this.billdetails = JSON.parse(localStorage  .getItem("billdata"));
+    debugger;
+    if(this.billdetails){
+      this.dataSource = new MatTableDataSource(this.billdetails.body);
+    }
+    
    }
- 
+   exportAsPDF(div_id)
+  {
+    let data = document.getElementById(div_id);  
+    html2canvas(data).then(canvas => {
+      var margin = 15;
+      var imgWidth = 210 - 2*margin; 
+      var pageHeight = 295 + 2*margin;  
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
+
+      var doc = new jspdf('p', 'mm');
+      var positiony = 10;
+      var positionx = 10;
+
+      doc.addImage(canvas, 'PNG', positionx, positiony, imgWidth, imgHeight);
+      heightLeft -= pageHeight;
+
+      while (heightLeft >= 0) {
+        positiony = heightLeft - imgHeight;
+        doc.addPage();
+        doc.addImage(canvas, 'PNG', positionx, positiony, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+      }
+      doc.save( 'file.pdf');
+    }); 
+  }
 }
