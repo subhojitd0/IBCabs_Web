6943b81f00267f86cdd3f39f941e649dcabcb3f4
SouradeepG <41611584+SouradeepG@users.noreply.github.com>
2021-05-28 00:55:30 +0530

    moment js added

--- src/app/features/check-ddr/checkddr.component.ts
+++ src/app/features/check-ddr/checkddr.component.ts
@@ -10,7 +10,8 @@ import { Router } from '@angular/router';
 import jspdf from 'jspdf';
 import html2canvas from 'html2canvas';
 import { ROUTE_CAR, ROUTE_GENERATE_BILL, ROUTE_OWNER } from 'src/shared/constants/constant';
-
+import * as moment from 'moment-timezone';
+//const moment = require('moment-timezone');
 export interface iBillDet {
   sl: string;
   dutydate: string;
@@ -67,6 +68,7 @@ export class CheckDdrComponent implements OnInit {
   month: string;
   filterby: string;
   filtervalue: string;
+  dataset: any;
   constructor(private router: Router,private apiService: ApiService, public dialog: MatDialog, private toastr: ToastrService) {
     
    }
@@ -86,6 +88,16 @@ export class CheckDdrComponent implements OnInit {
     };
     this.apiService.post(RENTAL_DETAIL_API_OFFICE, json).then((res: any)=>{ 
       debugger;
+      this.dataset = res.result;
+      this.dataset.forEach(element => { 
+        debugger;
+        let date1 =  moment.tz(element.gintime, "HH:mm:ss").local();
+        let date2 =  moment.tz(element.gouttime, "HH:mm:ss").local();
+        var x = moment.duration(date1.diff(date2)).asHours();
+        element.tothr = moment.duration(moment(element.gintime, "HH:mm:ss").diff(moment(element.gouttime, "HH:mm:ss"))).asMinutes() / 60;
+        element.totkm  = parseInt(element.ginkm) === NaN ? 0 : element.ginkm - parseInt(element.goutkm) === NaN ? 0 : element.goutkm;
+        element.outs = isNaN(parseInt(element.outstation)) ? 0 : parseInt(element.outstation);
+      });
       this.dataSource = new MatTableDataSource(res.result);
     });
     
