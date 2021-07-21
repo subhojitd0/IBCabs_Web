179a2228da93a4e6fa30420f5e090a7aed19f9be
Souradeep <ghoshouradeep475@gmail.com>
2021-07-21 22:25:27 +0530

    chges

--- src/app/features/edit-rental-detail/edit-rental-detail.component.ts
+++ src/app/features/edit-rental-detail/edit-rental-detail.component.ts
@@ -92,7 +92,7 @@ export class EditRentalDetailComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   displayedColumns: string[] = ['dutydt', 'party', 'carno', 'statusval', 'options'];
-  bulkdisplayedColumns: string[] = ['isselected','dutydate', 'partyname', 'reportto', 'driver', 'carnumber', 'cartype', 'slip', 'gouttime', 'goutkm', 'gintime', 'ginkm', 'routtime', 'routkm', 'rintime', 'rinkm', 'parking', 'outstation'];
+  bulkdisplayedColumns: string[] = ['isselected','dutydate', 'partyname', 'reportto', 'driver', 'carnumber', 'cartype', 'slip', 'gouttime', 'goutkm', 'routtime', 'routkm', 'rintime', 'rinkm', 'gintime', 'ginkm', 'parking', 'outstation'];
   dataSource: MatTableDataSource<RentalDetail>;
   bulkDataSource: MatTableDataSource<EditRentalDetail>;
   filteredOptionsCar: Observable<any[]>;
